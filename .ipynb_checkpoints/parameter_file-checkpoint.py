52a4ee90a6140824884d6d3a00c7b019bc5d7bd4
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2018-07-12 21:29:09 +0930

    started RDKIT notebook. Calculated PMIs and converted SMILES to 3D conformers

---
+++ .ipynb_checkpoints/parameter_file-checkpoint.py
@@ -0,0 +1,35 @@
+def get_parameters():
+    """
+    Get parameter dictionary
+    """
+    param_dict = {
+        # where all the brenda data files are
+        'BRENDA_DB_loc': '/home/atarzia/psp/brenda_details/',
+        'out_CSV_pi': "output_data_pi.csv",
+        'out_columns_pi': ['fasta_file', 'acc.code',
+                           'organism', 'EC.code', 'species',
+                           'note', 'pi', 'modification', 'category'],
+        'out_CSV_br': "output_data_br.csv",
+        'out_columns_br': ['fasta_file'],
+        # cut off for ZIF growth from pI
+        'cutoff_pi': 6,
+        # modification types + colours
+        # just implement succinylation for now
+        # why would you want to do acetylation if you can succinylate??
+        # currently succinylation == LYS swapped with GLU
+        'modifications': {
+            '0': {
+                'colour': 'k',
+                'name': 'unmodified',
+            },
+            '1': {
+                'colour': 'firebrick',
+                'name': 'succinylated',
+                'target_res': 'LYS',
+                'replace_res': 'GLU',
+            }
+        }
+
+    }
+
+    return param_dict
