aa10dbdcbc0ee72273c2e4f1cca9b77c2a6a2b73
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2018-09-15 17:45:37 +0930

    added general plotting functions

--- plotting.py
+++ plotting.py
@@ -10,6 +10,10 @@ Author: Andrew Tarzia
 Date Created: 15 Sep 2018
 
 """
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 
 def define_diff_categ_plot(ax, title, ytitle, xtitle, xlim, ylim):
@@ -39,3 +43,117 @@ def define_standard_plot(ax, title, ytitle, xtitle, xlim, ylim):
     # ax.legend([y, n], ['aligned', 'not aligned'], loc=4, fancybox=True)
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
+
+
+def print_results(molecules, threshold, output_dir):
+    """Print calculated ability to diffuse for all molecules in dictionary.
+
+    """
+    diffuse = {}
+    no_diffuse = {}
+    for name, smile in molecules.items():
+        out_file = output_dir+name.replace(' ', '_')+'_diam_result.csv'
+        print(out_file)
+        if os.path.isfile(out_file) is False:
+            continue
+        results = pd.read_csv(out_file)
+        min_diam = min(results['diam1'])
+        mid_diam = min(results['diam2'])
+        if mid_diam <= threshold:
+            print(name+':')
+            print('can diffuse')
+            print('min diameter =', round(min_diam, 3), 'angstrom')
+            print('mid diameter =', round(mid_diam, 3), 'angstrom')
+            diffuse[name] = smile
+        else:
+            print(name+':')
+            print('cannot diffuse')
+            print('min diameter =', round(min_diam, 3), 'angstrom')
+            print('mid diameter =', round(mid_diam, 3), 'angstrom')
+            no_diffuse[name] = smile
+        print('-')
+
+
+def categorical(molecules, threshold, output_dir):
+    """Categorical scatter plot of all molecules in dictionary.
+
+    """
+    dx = 0.15
+    fig, ax = plt.subplots(figsize=(5, 5))
+    for name, smile in molecules.items():
+        out_file = output_dir+name.replace(' ', '_')+'_diam_result.csv'
+        if os.path.isfile(out_file) is False:
+            continue
+        results = pd.read_csv(out_file)
+        mid_diam = min(results['diam2'])
+        if mid_diam <= threshold:
+            C = 'b'
+            M = 'o'
+            E = 'k'
+            D = 0.25
+        else:
+            C = 'r'
+            M = 'o'
+            E = 'k'
+            D = 0.75
+
+        ax.scatter(D+(dx*(np.random.random() - 0.5) * 2),
+                   mid_diam, c=C,
+                   edgecolors=E, marker=M, alpha=1.0,
+                   s=80)
+
+    ax.axhline(y=threshold, c='k')
+
+    define_diff_categ_plot(
+                        ax,
+                        title='',
+                        xtitle='',
+                        ytitle='intermediate diameter [$\mathrm{\AA}$]',
+                        xlim=(0, 1),
+                        ylim=(0, 10))
+    fig.tight_layout()
+    fig.savefig(output_dir+"categorical.pdf", dpi=720,
+                bbox_inches='tight')
+
+
+def shapes(molecules, threshold, output_dir):
+    """Plot molecule shapes of all molecules in dictionary.
+
+    """
+    fig, ax = plt.subplots(figsize=(5, 5))
+    for name, smile in molecules.items():
+        out_file = output_dir+name.replace(' ', '_')+'_diam_result.csv'
+        if os.path.isfile(out_file) is False:
+            continue
+        results = pd.read_csv(out_file)
+        mid_diam = min(results['diam2'])
+        if mid_diam <= threshold:
+            C = 'b'
+            M = 'o'
+            E = 'k'
+        else:
+            C = 'r'
+            M = 'o'
+            E = 'k'
+
+        ax.scatter(np.average(results['ratio_1']),
+                   np.average(results['ratio_2']),
+                   c=C,
+                   edgecolors=E, marker=M, alpha=1.0,
+                   s=80)
+
+    ax.plot([0, 0.5, 1, 0], [1, 0.5, 1, 1], c='k', lw=2)
+    ax.text(0.75, 1.03, 'sphere', fontsize=20)
+    ax.text(0.4, 0.45, 'oblate', fontsize=20)
+    ax.text(-0.05, 1.03, 'prolate', fontsize=20)
+
+    define_standard_plot(
+                        ax,
+                        title='',
+                        xtitle='$I_1$ / $I_3$',
+                        ytitle='$I_2$ / $I_3$',
+                        xlim=(-0.1, 1.1),
+                        ylim=(0.4, 1.1))
+    fig.tight_layout()
+    fig.savefig(output_dir+"shape.pdf", dpi=720,
+                bbox_inches='tight')
