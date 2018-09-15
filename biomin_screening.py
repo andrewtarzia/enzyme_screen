9944bc253f5a8a5c3df98388dfeaaeb8a10a08d6
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2018-09-15 15:09:44 +0930

    created main running scripts

---
+++ biomin_screening.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Script to run screening for biomineralisation literature reactions.
+
+Author: Andrew Tarzia
+
+Date Created: 15 Sep 2018
+
+"""
---
+++ param_screening.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Distributed under the terms of the MIT License.
+
+"""
+Script to run screening of parameterisation molecule data set.
+
+Author: Andrew Tarzia
+
+Date Created: 15 Sep 2018
+
+"""
