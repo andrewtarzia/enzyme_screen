d3462d798a7cd43bbfc32451b5d2046b2b2014e2
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2018-09-25 20:10:30 +0930

    new property plots

--- plotting.py
+++ plotting.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 import os
 from rxn_syst import yield_rxn_syst
 from rdkit.Chem import Descriptors
+import matplotlib.colors as colors
+import matplotlib.cm as cm
 
 
 def EC_descriptions():
@@ -306,12 +308,95 @@ def rs_pI_distribution(output_dir, cutoff_pI):
                 dpi=720, bbox_inches='tight')
 
 
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+    '''
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero
+
+
+    From Stack Exchange:
+        https://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+
+    Input
+    -----
+      cmap : The matplotlib colormap to be altered
+      start : Offset from lowest point in the colormap's range.
+          Defaults to 0.0 (no lower ofset). Should be between
+          0.0 and `midpoint`.
+      midpoint : The new center of the colormap. Defaults to
+          0.5 (no shift). Should be between 0.0 and 1.0. In
+          general, this should be  1 - vmax/(vmax + abs(vmin))
+          For example if your data range from -15.0 to +5.0 and
+          you want the center of the colormap at 0.0, `midpoint`
+          should be set to  1 - 5/(5 + 15)) or 0.75
+      stop : Offset from highets point in the colormap's range.
+          Defaults to 1.0 (no upper ofset). Should be between
+          `midpoint` and 1.0.
+    '''
+    cdict = {
+        'red': [],
+        'green': [],
+        'blue': [],
+        'alpha': []
+    }
+
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
+
+    # shifted index to match the data
+    shift_index = np.hstack([
+        np.linspace(0.0, midpoint, 128, endpoint=False),
+        np.linspace(midpoint, 1.0, 129, endpoint=True)
+    ])
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap(ri)
+
+        cdict['red'].append((si, r, r))
+        cdict['green'].append((si, g, g))
+        cdict['blue'].append((si, b, b))
+        cdict['alpha'].append((si, a, a))
+
+    newcmap = colors.LinearSegmentedColormap(name, cdict)
+    plt.register_cmap(cmap=newcmap)
+
+    return newcmap
+
+
+def define_plot_cmap(fig, ax, mid_point, cmap, ticks, labels, cmap_label):
+    """Define cmap shifted to midpoint and plot colourbar
+
+    """
+    new_cmap = shiftedColorMap(cmap, start=0, midpoint=mid_point,
+                               stop=1, name='shifted')
+
+    X = np.linspace(0, 1, 256)
+
+    cax = ax.scatter(-X-100, -X-100, c=X, cmap=new_cmap)
+
+    cbar = fig.colorbar(cax, ticks=ticks,
+                        spacing='proportional')
+    cbar.ax.set_yticklabels(labels,
+                            fontsize=16)
+    cbar.set_label(cmap_label, fontsize=16)
+
+    return new_cmap
+
+
 def rs_size_vs_SA_vs_logP(output_dir, size_thresh):
     """Plot maximum component size of a reaction vs. logP with synth access as
     3rd variable.
 
+    Plot max_logP => most hydroXX component.
+
     """
+    HRP_logP = 0.017
     fig, ax = plt.subplots(figsize=(8, 5))
+    new_cmap = define_plot_cmap(fig, ax, mid_point=0.5, cmap=cm.RdBu,
+                                ticks=[0, 0.25, 0.5, 0.75, 1],
+                                labels=['-10', '-5', '0', '5', '10'],
+                                cmap_label='$\Delta$ solvent accessibility')
     # iterate over reaction system files
     for rs in yield_rxn_syst(output_dir):
         if rs.skip_rxn is True:
@@ -319,9 +404,9 @@ def rs_size_vs_SA_vs_logP(output_dir, size_thresh):
         M = 'o'
         E = 'k'
 
-        ax.scatter(rs.pI,
+        ax.scatter(rs.max_logP,
                    rs.max_comp_size,
-                   c=C,
+                   c=new_cmap(abs((-10-rs.delta_sa))/20),
                    edgecolors=E,
                    marker=M,
                    alpha=1.0,
@@ -329,17 +414,20 @@ def rs_size_vs_SA_vs_logP(output_dir, size_thresh):
 
     # colour map
 
-    ax.axhline(y=size_thresh, c='k')
-    ax.axvline(x=HRP_logP, c='k')
+    ax.axhline(y=size_thresh, c='gray', linestyle='--')
+    ax.axvline(x=HRP_logP, c='gray', linestyle='--')
     define_standard_plot(ax,
                          title='',
-                         xtitle='logP',
+                         xtitle='logP of most hydrophobic component',
                          ytitle='diameter of largest component [$\mathrm{\AA}$]',
-                         xlim=(0, 14),
-                         ylim=(0, 10))
+                         xlim=(-6, 6),
+                         ylim=(0, 15))
     fig.tight_layout()
     fig.savefig(output_dir+"size_vs_SA_vs_logP.pdf", dpi=720,
                 bbox_inches='tight')
+
+    import sys
+    sys.exit()
 
 
 def rs_size_vs_pI(output_dir, cutoff_pI, size_thresh):
@@ -650,6 +738,44 @@ def rs_no_products(output_dir):
 
     fig.tight_layout()
     fig.savefig(output_dir+"no_prods_dist.pdf",
+                dpi=720, bbox_inches='tight')
+
+
+def rs_dist_deltaSA(output_dir):
+    """Plot distribution of the change in synthetic accesibility from react to
+    products.
+
+    """
+    fig, ax = plt.subplots(figsize=(8, 5))
+    delta = {}
+    # iterate over reaction system files
+    for rs in yield_rxn_syst(output_dir):
+        if rs.skip_rxn is True:
+            continue
+        if rs.delta_sa is not None:
+            top_EC = rs.EC.split('.')[0]
+            if top_EC not in list(delta.keys()):
+                delta[top_EC] = []
+            delta[top_EC].append(rs.delta_sa)
+
+    for keys, values in delta.items():
+        ax.hist(values,
+                facecolor=EC_descriptions()[keys][1],
+                alpha=0.4,
+                histtype='stepfilled',
+                bins=np.arange(-10, 10.2, 0.5),
+                label=EC_descriptions()[keys][0])
+
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.set_xlabel('$\Delta$ synthetic accessibility',
+                  fontsize=16)
+    ax.set_ylabel('count', fontsize=16)
+    ax.set_xlim(-10, 10)
+    # legend
+    ax.legend(fontsize=16)
+
+    fig.tight_layout()
+    fig.savefig(output_dir+"delta_SA_dist.pdf",
                 dpi=720, bbox_inches='tight')
 
 
