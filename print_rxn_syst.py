fe752565d8a49eb24bd690ee29d3012d105dd8f1
Andrew Tarzia <andrew.tarzia@adelaide.edu.au>
2018-10-08 17:36:11 +1030

    added use of compressed pickle files

--- print_rxn_syst.py
+++ print_rxn_syst.py
@@ -10,21 +10,18 @@ Author: Andrew Tarzia
 Date Created: 05 Sep 2018
 
 """
-# ensure cpickle usage
-try:
-    import cPickle as pickle
-except ModuleNotFoundError:
-    import pickle
+import pickle
+import bz2
 import sys
 
 if (not len(sys.argv) == 2):
     print('Usage: print_rxn_syst.py file\n')
-    print('    file: pickle file containing reaction system\n')
+    print('    file: compressed (.bpkl) pickled reaction system\n')
     sys.exit()
 else:
     file = sys.argv[1]
 
-with open(file, 'rb') as input:
+with bz2.BZ2File(file, 'rb') as input:
     rs = pickle.load(input)
 
 rs.print_rxn_system()
@@ -32,6 +29,5 @@ rs.print_rxn_system()
 try:
     print('all fit?', rs.all_fit)
     print('largest component size?', rs.max_comp_size)
-    print('seed MOF?', rs.seed_MOF)
 except AttributeError:
     pass
