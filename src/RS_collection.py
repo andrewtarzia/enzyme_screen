839f5ee69163a2870a80d7d063e87294fda46572
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-11-25 01:42:23 +0000

    clean up collection script. implement KEGG API

--- src/RS_collection.py
+++ src/RS_collection.py
@@ -13,84 +13,88 @@ Date Created: 05 Sep 2018
 
 import time
 from multiprocessing import Pool
-import pickle
-import gzip
 import glob
-from os.path import exists, join
+from os.path import join
 from os import getcwd
 import pandas as pd
 import sys
 
-from molecule import read_molecule_lookup_file, write_lookup_files
+import IO
 import utilities
+import reaction
+import KEGG_IO
 
 
-def get_reaction_systems(EC, DB, output_dir, molecule_dataset,
-                         clean_system=False,
-                         verbose=False):
-    """Get reaction system from SABIO reaction ID (rID).
+def get_reaction_systems(
+    EC,
+    DB,
+    params,
+    output_dir,
+    molecule_dataset,
+    clean_system=False,
+    verbose=False
+):
+    """
+    Get reaction system from SABIO reaction ID (rID).
 
     Keywords:
-        EC (str) - Enzyme commision number (X.X.X.X)
-        DB (str) - name of Database
-        output_dir (str) - directory where all data should be saved
-        molecule_dataset (Pandas DataFrame) -
-        look up for known molecules
-        clean_system (bool) - wipe the data in reaction systems
-        for fresh start
-            default = False
-        verbose (bool) - print update
-            default = False
+        EC (str) :
+            Enzyme commision number (X.X.X.X)
+        DB (str) :
+            name of Database
+        output_dir (str) :
+            directory where all data should be saved
+        molecule_dataset (pd.DataFrame) :
+            dataset of existing molecules to use
+        clean_system (bool) :
+            wipe the data in reaction systems for fresh start
+            (default = False)
+        verbose (bool) :
+            print update to terminal
+            (default = False)
 
     """
+
     if DB == 'SABIO':
-        print('SABIO DB cannot be used at this current time.')
-        print(' - KEGG OR ATLAS only')
-        sys.exit('exitting.')
-        from ercollect.SABIO_IO import get_rxn_systems
+        raise NotImplementedError(
+            'SABIO DB cannot be used at this current time.'
+        )
+        # rxn_fn = SABIO_IO.get_rxn_systems
     elif DB == 'KEGG':
         print('searching for EC:', EC)
-        from ercollect.KEGG_IO import get_rxn_systems
+        rxn_fn = KEGG_IO.get_rxn_systems
     elif DB == 'BKMS':
         print('BKMS DB cannot be used at this current time.')
         print(' - KEGG OR ATLAS only')
-        sys.exit('exitting.')
-        from ercollect.BKMS_IO import get_rxn_systems
+        raise NotImplementedError(
+            'BKMS DB cannot be used at this current time.'
+        )
+        # rxn_fn = BKMS_IO.get_rxn_systems
     elif DB == 'BRENDA':
-        print('BRENDA DB cannot be used at this current time.')
-        print(' - KEGG OR ATLAS only')
-        sys.exit('exitting.')
-        from ercollect.BRENDA_IO import get_rxn_systems
+        raise NotImplementedError(
+            'BRENDA DB cannot be used at this current time.'
+        )
+        # rxn_fn = BRENDA_IO.get_rxn_systems
     elif DB == 'ATLAS':
+        raise NotImplementedError(
+            'ATLAS DB cannot be used at this current time.'
+        )
         print('searching for EC:', EC)
-        from ercollect.ATLAS_IO import get_rxn_systems
-    get_rxn_systems(EC, output_dir, molecule_dataset=molecule_dataset,
-                    clean_system=clean_system,
-                    verbose=verbose)
+        # rxn_fn = ATLAS_IO.get_rxn_systems
 
-
-def get_RS(filename, output_dir, verbose=False):
-    """Read in reaction system from filename.
-
-    """
-    _rsf = filename.replace(output_dir+'sRS-', '').replace('.gpkl', '')
-    EC_, DB, DB_ID = _rsf.split('-')
-    EC = EC_.replace("_", ".").replace('XX', '-')
-    rs = reaction(EC, DB, DB_ID)
-    if isfile(output_dir+rs.pkl) is False:
-        print('you have not collected all reaction systems.')
-        print('Exitting.')
-        import sys
-        sys.exit()
-    # load in rxn system
-    if verbose:
-        print('loading:', rs.pkl)
-    rs = rs.load_object(output_dir+rs.pkl, verbose=False)
-    return rs
+    rxn_fn(
+        EC,
+        output_dir,
+        params=params,
+        molecule_dataset=molecule_dataset,
+        clean_system=clean_system,
+        verbose=verbose
+    )
 
 
 def percent_skipped(output_dir):
-    """Print the percent of all reaction systems that will NOT be skipped.
+    """
+    Print the percent of all reaction systems that will NOT be skipped.
 
     """
     # what percentage of reaction systems have skip_rxn = False
@@ -106,7 +110,7 @@ def percent_skipped(output_dir):
     rsf_bkms = glob.glob(output_dir+'sRS-*BKMS*.gpkl')
     rsf_kegg = glob.glob(output_dir+'sRS-*KEGG*.gpkl')
     rsf_sabio = glob.glob(output_dir+'sRS-*SABIO*.gpkl')
-    for rs in yield_rxn_syst(output_dir):
+    for rs in reaction.yield_rxn_syst(output_dir):
         if rs.skip_rxn is False:
             count += 1
             if 'ATLAS' in rs.pkl:
@@ -121,29 +125,42 @@ def percent_skipped(output_dir):
                 count_sabio += 1
 
     print('-----------------------------------')
-    print(count, 'reaction systems of', len(react_syst_files),
-          'are NOT skipped.')
+    print(
+        f'{count} reaction systems of '
+        f'{len(react_syst_files)} are NOT skipped.'
+    )
     print('=>', round(count/len(react_syst_files), 4)*100, 'percent')
     print('-----------------------------------')
-    print(count_atlas, 'reaction systems of', len(rsf_atlas),
-          'are NOT skipped in the ATLAS data set.')
+    print(
+        f'{count_atlas} reaction systems of '
+        f'{len(rsf_atlas)} are NOT skipped in the ATLAS data set.'
+    )
     print('-----------------------------------')
-    print(count_brenda, 'reaction systems of', len(rsf_brenda),
-          'are NOT skipped in the BRENDA data set.')
+    print(
+        f'{count_brenda} reaction systems of '
+        f'{len(rsf_brenda)} are NOT skipped in the BRENDA data set.'
+    )
     print('-----------------------------------')
-    print(count_bkms, 'reaction systems of', len(rsf_bkms),
-          'are NOT skipped in the BKMS data set.')
+    print(
+        f'{count_bkms} reaction systems of '
+        f'{len(rsf_bkms)} are NOT skipped in the BKMS data set.'
+    )
     print('-----------------------------------')
-    print(count_kegg, 'reaction systems of', len(rsf_kegg),
-          'are NOT skipped in the KEGG data set.')
+    print(
+        f'{count_kegg} reaction systems of '
+        f'{len(rsf_kegg)} are NOT skipped in the KEGG data set.'
+    )
     print('-----------------------------------')
-    print(count_sabio, 'reaction systems of', len(rsf_sabio),
-          'are NOT skipped in the SABIO data set.')
+    print(
+        f'{count_sabio} reaction systems of '
+        f'{len(rsf_sabio)} are NOT skipped in the SABIO data set.'
+    )
     print('-----------------------------------')
 
 
 def get_ECs_from_file(EC_file):
-    """Read in ECs to search from a file.
+    """
+    Read in ECs to search from a file.
 
     """
     # get search EC numbers from file:
@@ -194,8 +211,12 @@ def main_run(redo, pars):
     molecule_DB_directory = pars['molec_dir']
     lookup_file = join(molecule_DB_directory, 'lookup.txt')
     # write molecule look up files based on molecule DB
-    write_lookup_files(lookup_file, translator, molecule_DB_directory)
-    molecule_dataset = read_molecule_lookup_file(
+    IO.write_lookup_files(
+        lookup_file,
+        translator,
+        molecule_DB_directory
+    )
+    molecule_dataset = IO.read_molecule_lookup_file(
         lookup_file=lookup_file
     )
     print('--- settings:')
@@ -207,7 +228,6 @@ def main_run(redo, pars):
     print('--- collect all reaction systems (ONLINE)...')
     search_ECs = get_ECs_from_file(EC_file=search_EC_file)
     search_output_dir = getcwd()
-    sys.exit()
     for DB in search_DBs:
         # iterate over EC numbers of interest
         if NP > 1:
@@ -221,6 +241,7 @@ def main_run(redo, pars):
                     (
                         EC,
                         DB,
+                        pars,
                         search_output_dir,
                         molecule_dataset,
                         redo,
@@ -232,13 +253,21 @@ def main_run(redo, pars):
         # in serial
         else:
             for EC in search_ECs:
-                get_reaction_systems(EC=EC, DB=DB,
-                                     output_dir=search_output_dir,
-                                     molecule_dataset=molecule_dataset,
-                                     clean_system=redo, verbose=True)
+                get_reaction_systems(
+                    EC=EC,
+                    DB=DB,
+                    params=pars,
+                    output_dir=search_output_dir,
+                    molecule_dataset=molecule_dataset,
+                    clean_system=redo,
+                    verbose=True
+                )
     percent_skipped(search_output_dir)
-    print('---- time taken =', '{0:.2f}'.format(time.time()-temp_time),
-          's')
+    print(
+        '---- time taken =',
+        '{0:.2f}'.format(time.time()-temp_time),
+        's'
+    )
 
 
 def main():
