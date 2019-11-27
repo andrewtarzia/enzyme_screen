a27a04bd7af62ca17a60f8d3d4fcbc5541cac459
Andrew Tarzia <andrew.tarzia@gmail.com>
2019-11-27 00:59:22 +0000

    implement params and molecular population

--- src/molecule_population.py
+++ src/molecule_population.py
@@ -11,91 +11,67 @@ Date Created: 05 Sep 2018
 
 """
 
-from os import getcwd
-from ercollect import rxn_syst
-from plotting import (
-    mol_SA_vs_compl,
-    mol_SA_vs_NRB,
-    mol_SA_vs_NHA,
-    mol_logP_vs_logS,
-    mol_all_dist,
-    mol_logP_vs_XlogP
-)
+from os.path import exists
 import sys
+import glob
+import json
+from rdkit.Chem import AllChem as Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem.rdMolDescriptors import CalcNumRotatableBonds
 
+import IO
+import rdkit_functions as rdkf
+import plotting_fn
+import utilities
+
+
+def populate_all_molecules(params, redo, mol_file=None):
+    """
+    Populate all molecules in pickle files in directory.
+
+    """
+
+    vdwScale = params['vdwScale']
+    boxMargin = params['boxMargin']
+    spacing = params['spacing']
+    show_vdw = params['show_vdw']
+    plot_ellip = params['plot_ellip']
+    N_conformers = int(params['N_conformers'])
+    MW_thresh = params['MW_thresh']
+    seed = int(params['seed'])
+
+    fail_list = IO.fail_list_read(
+        directory=params['molec_dir'],
+        file_name='failures.txt'
+    )
+
+    if mol_file is None:
+        molecule_list = glob.glob('*_unopt.mol')
+    else:
+        molecule_list = IO.read_molecule_list(mol_file)
+
+    print(f'{len(molecule_list)} molecules in DB.')
 
 def main():
     if (not len(sys.argv) == 5):
         print("""
-Usage: molecule.py get_mol pop_mol mol_file
-    get_mol: T for overwrite and collection of molecules from RS in
-        current dir
-        (F for skip) ---- this function is useful if you update the
-        base attributes of the molecule class.
-    scratch: T for restart collection of molecules from RS.
-    pop_mol: T to run population of molecule properties
-        (does not overwrite).
-    mol_file: file name of list of molecules, F if not specified.
+Usage: molecule_population.py param_file redo mol_file
+    param_file:
+    redo:
+        t to overwrite all rxn systems.
+    plot:
+        t to plot distributions of molecule properties.
+    mol_file :
+        file name of list of molecules to allow for trivial
+        parallelisation, `f` if not specified, where all `mol`
+        files are populated.
 """)
         sys.exit()
     else:
-        get_mol = sys.argv[1]
-        scratch = sys.argv[2]
-        pop_mol = sys.argv[3]
-        mol_file = sys.argv[4]
-
-    if get_mol == 'T':
-        print(
-            'extract all molecules from reaction systems in '
-            'current dir...'
-        )
-        curr_dir = getcwd()
-        done_file = curr_dir+'/done_RS.txt'
-        lookup_file = (
-            '/home/atarzia/psp/molecule_DBs/atarzia/lookup.txt'
-        )
-        molecule_dataset = read_molecule_lookup_file(
-            lookup_file=lookup_file
-        )
-        update_molecule_DB(
-            rxn_syst.yield_rxn_syst(curr_dir+'/'),
-            from_scratch=scratch,
-            dataset=molecule_dataset,
-            done_file=done_file
-        )
-
-    if pop_mol == 'T':
-        vdwScale = 0.8
-        boxMargin = 4.0
-        spacing = 0.5
-        N_conformers = 100
-        MW_thresh = 500
-        print('settings:')
-        print('    VDW scale:', vdwScale)
-        print('    Box Margin:', boxMargin, 'Angstrom')
-        print('    Grid spacing:', spacing, 'Angstrom')
-        print('    No Conformers:', N_conformers)
-        print('    MW threshold:', MW_thresh, 'g/mol')
-        print('    Molecule file:', mol_file)
-        # inp = input('happy with these? (T/F)')
-        # if inp == 'F':
-        #     sys.exit('change them in the source code')
-        # elif inp != 'T':
-        #     sys.exit('I dont understand, T or F?')
-        print(
-            'populate the properties attributes for all '
-            'molecules in DB...'
-        )
-        directory = '/home/atarzia/psp/molecule_DBs/atarzia/'
-        if mol_file == 'F':
-            mol_file = False
-        populate_all_molecules(directory=directory,
-                               vdwScale=vdwScale,
-                               boxMargin=boxMargin,
-                               spacing=spacing,
-                               N_conformers=N_conformers,
-                               MW_thresh=MW_thresh,
-                               mol_file=mol_file)
+        params = utilities.read_params(sys.argv[1])
+        redo = True if sys.argv[2] == 't' else False
+        plot = True if sys.argv[3] == 't' else False
+        mol_file = None if sys.argv[4] == 'f' else sys.argv[4]
 
     if input('do plots? (t/f)') == 't':
         directory = '/home/atarzia/psp/molecule_DBs/atarzia/'
@@ -114,6 +90,15 @@ Usage: molecule.py get_mol pop_mol mol_file
         # # plot distributions of attributes
         mol_all_dist(output_dir=directory, plot_suffix='mol_cf')
     sys.exit()
+    print('settings:')
+    print('    Molecule file:', mol_file)
+    print(
+        'populate the properties attributes for all '
+        'molecules in DB...'
+    )
+
+    populate_all_molecules(params=params, mol_file=mol_file, redo=redo)
+
 
 
 if __name__ == "__main__":
